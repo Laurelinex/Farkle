f728762057e89d80a402a2b0b587b59fb2569505
Helen <aguda.helen@gmail.com>
2021-02-27 10:41:56 +0000

    components rendered to the screen

---
+++ client/src/components/Instruction.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Instructions = () => {
+    return(
+        <p className="instructions">This is where the instructions will go</p>
+    )
+    
+}
+
+
+
+
+export default Instructions;
