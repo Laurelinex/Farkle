b9e67eb07d8550e70f3251b444729a7345b5bf91
Helen <aguda.helen@gmail.com>
2021-02-27 10:55:56 +0000

    roll add

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,10 +1,38 @@
 import React from'react';
+import { useState } from 'react';
 
 const Game = () => {
 
-    return(
-        <h1>This is Farkle</h1>
-    );
+    const[randomNumber, setRandomNumber] = useState(0)
+        const onClickButton = () => {
+        const diceCode = getDiceCode(6);
+        setRandomNumber(diceCode);
+  }
+
+  function getDiceCode(length){
+    const code = [];
+    for (var i = 0; i < length; i++) {
+      var randomInt = getRandomInt(1, 7);
+      code.push(randomInt);
+    }
+    return code.join(" ");
+  }
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  
+  return (
+      <div className="dice-code">
+        <h1>Dice</h1>
+        <button className="button" onClick={onClickButton}>Roll Dice</button>
+        <h3>{randomNumber}</h3>
+      </div>
+  );
 }
+
 
 export default Game;
