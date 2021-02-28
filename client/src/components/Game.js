a7108b5c7e653bdae570f475bade6b566add85a6
Helen <aguda.helen@gmail.com>
2021-02-28 14:37:37 +0000

    Changed Game.js to support setting aside dice before rolling

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,42 +1,4 @@
 import React from'react';
-<<<<<<< HEAD
-import { useState, useEffect } from 'react';
-// import One from "../images/1.png";
-// import Two from "../images/2.png";
-// import Three from "../images/3.png";
-// import Four from "../images/4.png";
-// import Five from "../images/5.png";
-// import Six from "../images/6.png";
-// import css from "./game.css"
-
-const Game = () => {
-
-  const[diceArray, setDiceArray] = useState([]);
-  const[die, setDie] = useState({});
-
-  // const dicePicturesMap = {
-  //   1: <img src={One} />,
-  //   2: <img src={Two} />,
-  //   3: <img src={Three} />,
-  //   4: <img src={Four} />,
-  //   5: <img src={Five} />,
-  //   6: <img src={Six} />
-  // }
-
-  useEffect(() => {
-    setUpDiceArray()
-  }, []);
-
-  const setUpDiceArray = () => {
-    let arr = [];
-    for (let i = 0; i < 6; i++) {
-      diceArray[i] = {};
-      diceArray[i].id = 'id' + (i + 1);
-      diceArray[i].value = i + 1;
-      arr.push(diceArray[i])   
-    }
-    setDiceArray(arr)
-=======
 import { useState } from 'react';
 import One from "../images/1.png";
 import Two from "../images/2.png";
@@ -88,7 +50,6 @@ const Game = () => {
         dice[i].enabled = true;
     }
     return dice;
->>>>>>> feature/set-aside-dice
   }
 
  
@@ -108,29 +69,6 @@ const Game = () => {
   //   return diceArray;
   // }
         
-<<<<<<< HEAD
-
-
-  const rollDice = () => {
-    for (let i = 0; i < 6; i++) {
-      diceArray[i].value = Math.floor((Math.random() * 6) + 1);
-    }
-    diceArray.map((die) => {
-      return(
-        <li>die.value</li>
-      )
-    })  
-  }
-
-  return (
-      <div className="dice-code">
-        <h1>Dice</h1>
-
-        
-        <button className='button' onClick={onClickButton}>Roll Dice</button>
-
-
-=======
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -151,15 +89,11 @@ const Game = () => {
         <button className="button" onClick={onRollDice}>Roll Dice</button>
         <button className="button" onClick={() => setDiceArray(setupDiceArray(6))}>Reset</button>
         <DieList dice={diceArray} toggleActive={toggleActive} />
->>>>>>> feature/set-aside-dice
       </div>
   );
 }
 
 
-<<<<<<< HEAD
-export default Game;
-=======
 class DieList extends React.Component{
   render() {
     let _this = this;
@@ -185,4 +119,3 @@ class DieList extends React.Component{
 
 
 export default Game;
->>>>>>> feature/set-aside-dice
