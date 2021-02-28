7a0e13aad42f7f6b5788a60bc95c2950bb049e54
Helen <aguda.helen@gmail.com>
2021-02-28 14:29:40 +0000

    Changed Game.js to support setting aside dice before rolling

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
      
       <Game />
-      <Instructions />
+      {/* <Instructions /> */}
 
       {/* <Leaderboard /> */}
 
--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -6,71 +6,99 @@ import Three from "../images/3.png";
 import Four from "../images/4.png";
 import Five from "../images/5.png";
 import Six from "../images/6.png";
+import Tooltip from '@material-ui/core/Tooltip';
 import css from "./game.css"
 
 const Game = () => {
 
-    const dicePicturesMap = {
-      1: <img src={One} />,
-      2: <img src={Two} />,
-      3: <img src={Three} />,
-      4: <img src={Four} />,
-      5: <img src={Five} />,
-      6: <img src={Six} />
+  const dicePicturesMap = {
+    1: <button><img src={One}  /></button>,
+    2: <button><img src={Two}  /></button>,
+    3: <button><img src={Three}/></button>,
+    4: <button><img src={Four} /></button>,
+    5: <button><img src={Five} /></button>,
+    6: <button><img src={Six}  /></button>
+  }
+    
+  const[diceArray, setDiceArray] = useState(setupDiceArray(6));
+
+  const onRollDice = () => {
+    const numDiceAvailable = diceArray.filter(die => die.enabled === true).length
+    const dice = getDiceArray(numDiceAvailable);
+    console.log(dice);
+    setDiceArray(dice);
+  }
+
+  function setupDiceArray(numOfDice) {
+    let initDiceArray = [];
+    for (var i = 0; i < numOfDice; i++) {
+      initDiceArray[i] = {};
+      initDiceArray[i].value = dicePicturesMap[i + 1];
+      initDiceArray[i].id = "die" + (i+1);
+      initDiceArray[i].enabled = true;
     }
-      // 2: "2.png", 3: "3.png", 4: "4.png", 5: "5.png", 6: "6.png"}
-    
-    const[diceArray, setDiceArray] = useState([1, 2, 3, 4, 5, 6]);
-    
-
-
-
-        const onClickButton = () => {
-        const dice = getDiceArray(6);
-        console.log(dice);
-        setDiceArray(dice.map(die => dicePicturesMap[die]));
+    return initDiceArray;
   }
 
   function getDiceArray(length){
-    const diceArray = [];
+    const dice = [];
     for (var i = 0; i < length; i++) {
         var randomInt = getRandomInt(1, 7)
-        diceArray[i] = randomInt;
-        // const diceArray[i].id = i+1
-        // const diceArray[i].value = i+1;
-        // const diceArray[i].status = false;
+        dice[i] = {};
+        dice[i].id = "die" + (i+1);
+        dice[i].value = dicePicturesMap[randomInt];
+        dice[i].enabled = true;
     }
-    return diceArray;
+    return dice;
   }
         
-    // const randomInt = getRandomInt(1, 7){
-    //     for (var i=0; i < 6; i++) {
-    //         diceArray[i].value(randomInt)
-    // }
-
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
   }
 
-  // const loadPictures = diceArray.map(die => {
-  //   return (
-  //            <li >
-  //               {/* <img src={die} width="10px"/>     */}
-  //           </li>
-  //           )
-  // })
+  function toggleActive(selectedId) {
+    var newDiceArray = [].concat(diceArray); 
+    var die = newDiceArray.find(die => die.id === selectedId);
+    die.enabled = !die.enabled;
+    setDiceArray(newDiceArray);
   
+  }
+
   return (
       <div className="dice-code">
         <h1>Dice</h1>
-        <button className="button" onClick={onClickButton}>Roll Dice</button>
-        <h3>{diceArray}</h3>
-        
+        <button className="button" onClick={onRollDice}>Roll Dice</button>
+        <button className="button" onClick={() => setDiceArray(setupDiceArray(6))}>Reset</button>
+        <DieList dice={diceArray} toggleActive={toggleActive} />
       </div>
   );
 }
+
+
+class DieList extends React.Component{
+  render() {
+    let _this = this;
+    
+    return (
+      <ul className="dice-list">
+        {this.props.dice.map(function(die) {
+          
+          return (
+            <li key={die.id}>
+              <a href="#">{die.value}</a>
+              <span>{die.enabled ? 'Active' : 'Not Active'}</span>
+              <button onClick={_this.props.toggleActive.bind(null, die.id)} >
+                Set Aside
+              </button>
+            </li>
+            );
+        })}
+      </ul>
+      );
+  }
+};
 
 
 export default Game;
