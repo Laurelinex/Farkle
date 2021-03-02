04df306f254ba61e572c7ded485be23c8c985e9a
Helen <aguda.helen@gmail.com>
2021-03-02 20:32:32 +0000

    gamecss problem

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -6,7 +6,7 @@ import { FaDiceThree } from "react-icons/fa";
 import { FaDiceFour } from "react-icons/fa";
 import { FaDiceFive } from "react-icons/fa";
 import { FaDiceSix } from "react-icons/fa";
-import "./Game.css";
+import "./game.css";
 import Die from "./Die";
 
 const Game = () => {
