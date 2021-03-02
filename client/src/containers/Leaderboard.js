8eab9c334cc0715b3fec935d1d7b623867ed412d
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-03-02 20:40:38 +0000

    sort players by win f added

--- client/src/containers/Leaderboard.js
+++ client/src/containers/Leaderboard.js
@@ -6,9 +6,29 @@ const Leaderboard = () => {
 
     const [players, setPlayers] = useState([]);
 
+    const sortPlayersByWins = players => {
+        const len = players.length;
+        for (let i = len - 1; i >= 0; i--) {
+            for (let j = 1; j <= i; j++) {
+                
+                const playerScorex = players[j - 1].wins / players[j - 1].losses === Infinity ? players[j - 1].wins : players[j - 1].wins / players[j - 1].losses || 0;
+                const playerScorej = players[j].wins / players[j].losses === Infinity ? players[j].wins : players[j].wins / players[j].losses || 0;
+                if (playerScorex < playerScorej) {
+                    const temp = players[j - 1];
+                    players[j - 1] = players[j];
+                    players[j] = temp;
+                }
+            }
+        }
+        return players;
+    };
+
     useEffect(() => {
         fetchAll()
-        .then(players => {setPlayers(players));
+        .then(players => {
+            const sortedPlayers = sortPlayersByWins(players);
+            setPlayers(sortedPlayers);
+        });
     }, [])
 
     const playerList = players.map((player, index) => {
