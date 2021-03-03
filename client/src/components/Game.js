11e37db1f5269e7e9e9aba94d199e32c1d6f4d52
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-03-03 20:42:09 +0000

    cant figure out gamecss name issue

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,4 +1,5 @@
 import React from'react';
+import "./game.css";
 import { useState, useEffect } from 'react';
 import { FaDiceOne } from "react-icons/fa";
 import { FaDiceTwo } from "react-icons/fa";
@@ -13,7 +14,6 @@ import PlayerTwoSelector from './PlayerTwoSelector';
 // import PlayerTwo from './PlayerTwo';
 import PlayerForm from './PlayerForm';
 import {fetchAll, increaseWinOrLosses} from '../services/PlayersServices';
-import "./Game.css";
 
 const Game = () => {
 
